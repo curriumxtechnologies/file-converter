3b42b1fd695b394f5ee4bd5e96dc1bb87e2e0e5c
Joshua <sorochijoshua30@gmail.com>
2026-05-20 17:15:59 +0100

    update

--- backend/main.py
+++ backend/main.py
@@ -14,9 +14,7 @@ load_dotenv()
 ALLOWED_ORIGINS = [
     "https://fileconverter.curriumx.online",
     "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://127.0.0.1:3000",
-    "http://localhost:3000"
+    "http://localhost:5500"
 ]
 
 @asynccontextmanager
