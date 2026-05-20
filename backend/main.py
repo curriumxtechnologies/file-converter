ac805bb165f0f05ea41c7f255a556fb7e4605e2d
Joshua <sorochijoshua30@gmail.com>
2026-05-20 17:26:20 +0100

    update

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 # Allowed origins - production and local development
 ALLOWED_ORIGINS = [
-    "https://fileconverter.curriumx.online",
+    "https://filesconverter.curriumx.online",
     "http://127.0.0.1:5500",
     "http://localhost:5500"
 ]
